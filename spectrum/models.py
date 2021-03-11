6464c5ef889d6ac5554fb763c69f3d5225b864f6
Alexa <as.chigir@gmail.com>
2021-03-11 23:22:23 +0300

    Iteration 14
    Start add slider

--- spectrum/models.py
+++ spectrum/models.py
@@ -68,3 +68,28 @@ class Profile(models.Model):
                                 on_delete=models.CASCADE)
     birth = models.DateTimeField(blank=True, null=True)
     photo = models.ImageField(upload_to="user/%Y/%m/%d", blank=True)
+
+
+class Slider(models.Model):
+    name = models.CharField(max_length=100,
+                            help_text='просто название слайда до 100 символов, оно обязательно',
+                            verbose_name='Название слайда')
+    title = models.CharField(blank=True,
+                             max_length=150,
+                             help_text='до 150 символов, заполнять не обязательно',
+                             verbose_name='Заголовок на слайде')
+    description = models.CharField(blank=True,
+                                   max_length=150,
+                                   help_text='до 150 символов, заполнять не обязательно',
+                                   verbose_name='Текст под заголовком')
+    sliderimg = models.ImageField(blank=False,
+                                  upload_to='slider/',
+                                  help_text='изображение окажется до 1920*1024рх, поэтому лучше брать большое',
+                                  verbose_name='Добавить изображение')
+
+    class Meta:
+        verbose_name = 'Блок 1ю0: Слайдер'
+        verbose_name_plural = 'Блок 1ю0: Слайдеры'
+
+    def __str__(self):
+        return self.name
